ec8921d92fe723c6541af99b23b78a16ebfa1502
masontaylor7 <masonevantaylor@gmail.com>
2022-11-14 14:56:21 -0800

    added model product.py and created migration file

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from .db import db
 from .user import User
 from .address import Address
 from .user_address import User_Address
+from .product import Product
---
+++ app/models/product.py
@@ -0,0 +1,19 @@
+from .db import db
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'quantity': self.quantity,
+            'price': self.price,
+        }
